def9c3dd9235fa6fc1033746693b0e34b0e655c1
Chamali Wedage <94621140+ChamaliWedage-99@users.noreply.github.com>
2023-11-24 00:20:49 +0530

    some changes in app.js

--- app.js
+++ app.js
@@ -42,11 +42,67 @@ class UI{
 
         list.appendChild(row);
     }
+
+    static deleteBook(el){
+        if(el.classList.contains('delete'))
+        {
+            el.parentElement.parentElement.remove();
+        }
+    }
+
+    static showAlert(message, className){
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+    }
+
+    static clearFeilds()
+    {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
+    }
 }
 //Store Class : Handles storage
 // Event: Display Books
 
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 //Event: Add a book
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+   //prevent actual submit
+   e.preventDefault();
+   
+    //Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //validate
+    if(title ==='' || author === '' || isbn === '')
+{
+    UI.showAlert('Please fill in all the feidls!', 'danger');
+}
+else{
+     //instantiate book
+
+     const book = new Book(title, author, isbn);
+     console.log(book);
+ 
+     //Add book to UI
+     UI.addBookToList(book);
+     
+     //clear fields
+     UI.clearFeilds();
+}
+
+   
+});
 // Event: Remove a book
+document.querySelector('#book-list').addEventListener('click', (e) =>{
+    UI.deleteBook(e.target)
+});
 
